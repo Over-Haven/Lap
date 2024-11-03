f6b0d0e983a48799ab3d1140aa939d4145239222
Over-haven <tanushdas26@gmail.com>
2024-11-03 20:50:40 +0530

    Hellow

---
+++ main.py
@@ -0,0 +1,11 @@
+#Input
+String = input("Please enter your own string:")
+ 
+straing2 = ('')
+
+#Loop
+for i in String:
+ straing2 = i + straing2
+
+print("\nThe original string = ", String)
+print("The reversed string =", straing2)
